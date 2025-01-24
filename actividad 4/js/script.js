b375304b9e4716e272874441e2ce698540b9ad84
jacinto <jmarmat297@g.educaand.es>
2025-01-24 11:55:40 +0100

    actividad 4

---
+++ actividad 4/js/script.js
@@ -0,0 +1,10 @@
+
+
+function Calcular() {
+        a= parseInt(prompt("introduce un número:",10));
+        b= parseInt(prompt("introduce un número:",10));
+        let=a;
+        let=b;
+        alert("el primer numero es: " + a + "\nEl segundo numero es: " + b + "\nla suma es: " + ( a + b ) + "\nLa resta es: " + (a - b) + "\nel producto es: " + ( a * b ) + " \nla divioson es: " + ( a / b ) +"\nel resto es: " + ( a % b ));
+
+     }
