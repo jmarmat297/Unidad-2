51d804b09352321f0974e9159448af5d97b02f75
jacinto <jmarmat297@g.educaand.es>
2025-01-24 08:28:24 +0100

    Add files via upload

---
+++ actividad 3/js/script.js
@@ -0,0 +1,9 @@
+
+
+function pedirdatos() {
+        nombre=prompt("¿Como te llamas?")
+        edad=prompt("¿Que edad tienes?")
+        alert(" Hola soy " + nombre + " y tengo " + edad + " años")
+        let=nombre
+        let=edad
+     }
