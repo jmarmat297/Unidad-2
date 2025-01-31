06a05653a31790891bf837ae04ee174dc6598a4f
jacinto <jmarmat297@g.educaand.es>
2025-01-31 11:41:17 +0100

    ea

--- actividad 4/js/script.js
+++ actividad 4/js/script.js
@@ -3,8 +3,8 @@
 function Calcular() {
          let a;
         let b;
-        a= parseInt(prompt("introduce un número:",10));
-        b= parseInt(prompt("introduce un número:",10));
+        a= parseInt(prompt("introduce un número:"),10);
+        b= parseInt(prompt("introduce un número:"),10);
         
         alert("el primer numero es: " + a + "\nEl segundo numero es: " + b + "\nla suma es: " + ( a + b ) + "\nLa resta es: " + (a - b) + "\nel producto es: " + ( a * b ) + " \nla divioson es: " + ( a / b ) +"\nel resto es: " + ( a % b ));
 
