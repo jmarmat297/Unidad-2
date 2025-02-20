06cf59bb9f8a63dccaf0c40ddbc8e3d764956194
jacinto <jmarmat297@g.educaand.es>
2025-02-20 14:20:09 +0100

    Actividad 6 buena

--- actividad 6/js/script.js
+++ actividad 6/js/script.js
@@ -1,9 +1,12 @@
 function comprobar() {
-    let numero = parseInt(document.getElementById("Numero").value);
-
-    if (numero % 2 === 0) {
-        document.getElementById("mensaje").innerHTML = "El número es par"; 
-    } else {
-        document.getElementById("mensaje").innerHTML = "El número es impar";
+    let numero ;
+    numero = parseInt(document.getElementById ("numero").value);
+    
+    if (numero % 2 == 0 ) {
+       document.getElementById("mensaje").innerHTML = "El númeron es par";
     }
-}
+    else {
+       document.getElementById("mensaje").innerHTML = "El número es impar";
+    }
+        
+ }
