c839222ab6202905be3779f18b6c533926dd27a8
jacinto <jmarmat297@g.educaand.es>
2025-02-20 13:43:01 +0100

    ARREGLADA, QUE NO FUNCIONABA

--- actividad 5/js/script.js
+++ actividad 5/js/script.js
@@ -1,5 +1,5 @@
 function calcular() {
     let a = parseInt(document.getElementById("a").value, 10);
     let b = parseInt(document.getElementById("b").value, 10);
-    document.getElementById("resultado").innierHTML = "el primer numero es: " + a + "\nEl segundo numero es: " + b + "\nla suma es: " + ( a + b ) + "\nLa resta es: " + (a - b) + "\nel producto es: " + ( a * b ) + " \nla divioson es: " + ( a / b ) +"\nel resto es: " + ( a % b );
+    document.getElementById("resultado").innerHTML = "el primer numero es: " + a + "\nEl segundo numero es: " + b + "\nla suma es: " + ( a + b ) + "\nLa resta es: " + (a - b) + "\nel producto es: " + ( a * b ) + " \nla divioson es: " + ( a / b ) +"\nel resto es: " + ( a % b );
 }
